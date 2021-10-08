90008543b0a4dcd5e871a79087af5496926b6cd5
Edgar-84 <edgar.tomko.97@mail.ru>
2021-10-08 17:03:55 +0300

    create function wich create files with links on first 30 companies of category

--- main.py
+++ main.py
@@ -9,21 +9,22 @@ def find_name(url):
     name = name.split('/')[-1]
     return name
 
+HEADERS = {
+        'Accept': '*/*',
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0"
+}
+
 def get_categories_url(url):
     '''
     This function find all url categories companies
 
-    return: dict with list url categories
+    :return: dict with list url categories
     '''
-    headers = {
-        'Accept': '*/*',
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0"
-    }
 
-    reg = requests.get(url, headers=headers)
-
-    with open('obraz/categories.html', 'w') as file:
-        file.write(reg.text)
+    # reg = requests.get(url, headers=HEADERS)
+    #
+    # with open('obraz/categories.html', 'w') as file:
+    #     file.write(reg.text)
 
     with open('obraz/categories.html') as file:
         src = file.read()
@@ -33,9 +34,40 @@ def get_categories_url(url):
 
     category_companyes_urls = []
     for article in articles:
-        project_url = "https://rabota.by" + article.get('href') + '&vacanciesRequired=true'
+        project_url = "https://rabota.by" + article.get('href').split('?')[0] + '?page=0&vacanciesRequired=true&area=16'
         category_companyes_urls.append(project_url)
 
     return category_companyes_urls
 
-get_categories_url("https://rabota.by/employers_company?area=16")
+def get_urls_each_category():
+    """
+    This function save links companies for each category
+    :return: files with links companies
+    """
+
+    for cat_company_url in get_categories_url("https://rabota.by/employers_company"):
+        print(f"{cat_company_url} ####### successful")
+        reg = requests.get(cat_company_url, headers=HEADERS)
+        project_name = find_name(cat_company_url)
+
+        with open(f"obraz/{project_name}.html", 'w') as file:
+            file.write(reg.text)
+
+        with open(f'obraz/{project_name}.html') as file:
+            src = file.read()
+
+        soup = BeautifulSoup(src, 'lxml')
+        articles = soup.find_all("span", class_='employers-company__description')
+
+        companyes_urls = []
+        for article in articles:
+            company_url = "https://rabota.by" + article.find('a').get("href")
+            companyes_urls.append(company_url)
+
+        with open(f'data/{project_name}.txt', 'a') as file: # save links on companies in files
+            for line in companyes_urls:
+                file.write(f'{line}\n')
+
+
+get_urls_each_category()
+
